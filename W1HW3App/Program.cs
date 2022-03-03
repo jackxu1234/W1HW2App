fc9c3719f0108a5aeff9abd78121581b8085e46f
wx x <2785230875@qq.com>
2022-03-03 22:12:59 +0800

    添加项目文件。

---
+++ W1HW3App/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW3App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                leapyear LP = new leapyear();
+                LP.input();
+                if (LP.isleapyear())
+                {
+                    LP.printtrue();
+                }
+                else
+                {
+                    LP.printfalse();
+                }
+            }
+        }
+    }
+    class leapyear
+    {
+        int year;
+        public void input()
+        {
+            year = Convert.ToInt32(Console.ReadLine());
+        } 
+        public bool isleapyear()
+        {
+            
+            if ((0 == year % 4 && 0 != year % 100) || (0 == year % 400))
+            {
+                return true; 
+            }
+            else
+            {
+                return false;
+            }
+        } 
+        public void printtrue()
+        {
+            Console.WriteLine("{0} is a Leap 。\n", year);
+        } 
+        public void printfalse()
+        {
+            Console.WriteLine("{0}is not Leap。\n", year);
+        } 
+    }
+    }
+
